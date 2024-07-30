a9c72f0ea171b135d542bd7c67ac4734c892419d
GHjiejie <3426571530@qq.com>
2024-07-31 04:01:45 +0800

    补全

--- algorithm/dynamicProgramming/uniquePaths.js
+++ algorithm/dynamicProgramming/uniquePaths.js
@@ -8,16 +8,15 @@ var uniquePaths = function (m, n) {
     .fill()
     .map((item) => Array(n));
 
-  for (let i = 0; i < m; ++i) {
+  for (let i = 0; i < m; i++) {
     dp[i][0] = 1;
   }
-
-  for (let i = 0; i < n; ++i) {
+  for (let i = 0; i < n; i++) {
     dp[0][i] = 1;
   }
 
-  for (let i = 1; i < m; ++i) {
-    for (let j = 1; j < n; ++j) {
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
       dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
   }
---
+++ algorithm/interview/arr&string/6.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// var rotate = function (nums, k) {
+//   let sliceIndex = nums.length - k;
+//   let sliceArr = nums.splice(sliceIndex, k);
+//   return sliceArr.concat(nums);
+// };
+
+var rotate = function (nums, k) {};
+
+const nums = [-1, -100, 3, 99];
+const k = 2;
+
+console.log(rotate(nums, k));
